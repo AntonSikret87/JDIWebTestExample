11b6303f90f7be18c19bbf8eed4b5f4f169dcf1e
Anton Karabeinikov <a.karabeinikov@astoundcommerce.com>
2018-11-28 18:00:45 +0200

    update InitTests for grid

--- src/test/java/org/mytests/InitMyGmailTests.java
+++ src/test/java/org/mytests/InitMyGmailTests.java
@@ -1,6 +1,8 @@
 package org.mytests;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -11,23 +13,39 @@ import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.settings.WebSettings;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import org.mytests.gmail.com.site.GmailSite;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import static com.epam.commons.StringUtils.LINE_BREAK;
-import static com.epam.jdi.uitests.core.logger.LogLevels.STEP;
 import static com.epam.jdi.uitests.core.settings.JDISettings.logger;
 
 
 
 public class InitMyGmailTests extends TestNGBase {
+	WebDriver driver;
+
+//	@BeforeSuite()
+//	public static void setUp() throws Exception {
+//		logger.setLogLevel(STEP);
+//		WebSite.init(GmailSite.class);
+//		//GmailSite.open();
+//		logger.info("Run Tests");
+//	}
 
 
-	@BeforeSuite()
-	public static void setUp() throws Exception {
-		logger.setLogLevel(STEP);
+
+	@BeforeSuite
+	public void setUp() throws MalformedURLException {
+		String nodeURL = "http://AKARABEINIKOV-NB.astound.local:4445";
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		capability.setBrowserName("chrome");
+		capability.setPlatform(Platform.WIN10);
+		driver = new RemoteWebDriver(new URL(nodeURL), capability);
 		WebSite.init(GmailSite.class);
-		//GmailSite.open();
 		logger.info("Run Tests");
 	}
 
