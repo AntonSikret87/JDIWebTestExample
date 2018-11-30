fe610572298ce7e95e0575aa9914969b028793f1
Anton Karabeinikov <a.karabeinikov@astoundcommerce.com>
2018-11-30 15:39:15 +0200

    update pom.xml

--- src/test/java/org/mytests/InitMyGmailTests.java
+++ src/test/java/org/mytests/InitMyGmailTests.java
@@ -11,7 +11,9 @@ import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.settings.WebSettings;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import org.mytests.gmail.com.site.GmailSite;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterSuite;
@@ -20,7 +22,6 @@ import org.testng.annotations.BeforeSuite;
 import static com.epam.commons.StringUtils.LINE_BREAK;
 import static com.epam.jdi.uitests.core.settings.JDISettings.logger;
 import static com.epam.jdi.uitests.web.selenium.driver.WebDriverUtils.killAllRunWebBrowsers;
-import static com.epam.jdi.uitests.web.settings.WebSettings.useDriver;
 
 
 public class InitMyGmailTests extends TestNGBase {
@@ -34,53 +35,61 @@ public class InitMyGmailTests extends TestNGBase {
 //
 //	}
 
-//
-//	@BeforeSuite()
-//	public static void setUp() throws Exception {
-//		WebSettings.useDriver(() -> {
-//			DesiredCapabilities capabilities = new DesiredCapabilities();
-//			// ChromeOptions chromeOptions = new ChromeOptions();
-//			//capabilities.setBrowserName("chrome");
-//			// capabilities.setPlatform(Platform.WIN10);
-//			//capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
-//
-//			RemoteWebDriver driver = null;
-//			try {
-//				driver = new RemoteWebDriver(
-//						URI.create("http://192.168.65.1:5559/wd/hub").toURL(),
-//						capabilities
-//				);
+public static ThreadLocal<WebDriver> driverContainer = new ThreadLocal<>();
+//static RemoteWebDriver driver = null;
+	@BeforeSuite()
+	public static void setUp() throws Exception {
+		//System.setProperty("webdriver.chrome.driver","D:\\AutoMy\\Drivers\\chromedriver.exe");
+		//useDriver(() -> {
+
+
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			ChromeOptions chromeOptions = new ChromeOptions();
+			//chromeOptions.addArguments("--headless");
+			//capabilities.setBrowserName("chrome");
+			capabilities.setPlatform(Platform.WIN10);
+			//capabilities.setVersion("69.0.3497.0");
+			//capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+			capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+
+
+		//	try {
+				System.setProperty("webdriver.chrome.driver","D:\\AutoMy\\Drivers\\chromedriver.exe");
+				driverContainer.set(new RemoteWebDriver(
+						URI.create("http://localhost:4444/wd/hub").toURL(), //http://192.168.65.1:4444/wd/hub
+						capabilities));
+//				));
 //			} catch (MalformedURLException e) {
 //				e.printStackTrace();
 //			}
 //
-//			return driver;
+//			return driverContainer.get();
 //		});
-//
-//		WebSite.init(GmailSite.class);
-//		//GmailSite.open();
-//		logger.info("Run Tests");
-//	}
-public static ThreadLocal<WebDriver> driverContainer = new ThreadLocal<>();
-
-	@BeforeSuite(description = "Создание драйвера и инициализация сайта")
-	public static void setUpc() throws IOException {
-
-
-		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-		//capabilities.setBrowserName("chrome");
-		//capabilities.setPlatform(Platform.WIN10);
-		//capabilities.setVersion("62.0");
-		System.out.println(System.getProperty("driver"));
-		System.getProperty("driver");
-
-		driverContainer.set(new RemoteWebDriver(
-				URI.create("http://localhost:4444/wd/hub").toURL(),  //http://192.168.65.1:5558/wd/hub
-				capabilities
-		));
-
-		WebSite.init(useDriver(()->driverContainer.get()),GmailSite.class);
+		WebSite.init(GmailSite.class);
+		//GmailSite.open();
+		//GmailSite.open();
+		logger.info("Run Tests");
 	}
+//public static ThreadLocal<WebDriver> driverContainer = new ThreadLocal<>();
+//
+//	@BeforeSuite(description = "Создание драйвера и инициализация сайта")
+//	public static void setUpc() throws IOException {
+//
+//
+//		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+//		//capabilities.setBrowserName("chrome");
+//		capabilities.setPlatform(Platform.WIN10);
+//		//capabilities.setVersion("62.0");
+//		System.out.println(System.getProperty("driver"));
+//		System.getProperty("driver");
+//
+//		driverContainer.set(new RemoteWebDriver(
+//				URI.create("http://192.168.65.1:4444/wd/hub").toURL(),  //http://192.168.65.1:5558/wd/hub
+//				capabilities
+//		));
+//
+//		WebSite.init(useDriver(()->driverContainer.get()),GmailSite.class);
+//	}
 
 
 
